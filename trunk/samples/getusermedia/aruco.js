bc8d63ac43e8f46583d9e6594c14bd1485121f60
jcmellado@gmail.com <jcmellado@gmail.com@546bf43c-128a-a1a6-929b-a8dd843f23af>
2012-03-19 10:33:36 +0000

    - Added posit1 and posit2 to estimate 3d pose from 2d points - Used fast Stack Box Blur instead of slow Gaussian Blur - Added supersampling to warp function - Reworked loops, "for" seems now faster than "while" - Added new demo to debug POSIT algorithm - Updated old demos to use getUserMedia

--- trunk/samples/getusermedia/aruco.js
+++ trunk/samples/getusermedia/aruco.js
@@ -1,3 +1,30 @@
+/*
+Copyright (c) 2011 Juan Mellado
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+/*
+References:
+- "ArUco: a minimal library for Augmented Reality applications based on OpenCv"
+  http://www.uco.es/investiga/grupos/ava/node/26
+*/
 
 var AR = AR || {};
 
@@ -9,8 +36,8 @@ AR.Marker = function(id, corners){
 AR.Detector = function(){
   this.grey = new CV.Image();
   this.thres = new CV.Image();
-  this.mean = new CV.Image();
   this.homography = new CV.Image();
+  this.binary = [];
   this.contours = [];
   this.polys = [];
   this.candidates = [];
@@ -18,9 +45,9 @@ AR.Detector = function(){
 
 AR.Detector.prototype.detect = function(image){
   CV.grayscale(image, this.grey);
-  CV.adaptiveThreshold(this.grey, this.thres, this.mean, 7, 7);
+  CV.adaptiveThreshold(this.grey, this.thres, 2, 7);
   
-  this.contours = CV.findContours(this.thres);
+  this.contours = CV.findContours(this.thres, this.binary);
 
   this.candidates = this.findCandidates(this.contours, image.width * 0.20, 0.05, 10);
   this.candidates = this.clockwiseCorners(this.candidates);
@@ -30,11 +57,11 @@ AR.Detector.prototype.detect = function(image){
 };
 
 AR.Detector.prototype.findCandidates = function(contours, minSize, epsilon, minLength){
-  var candidates = [], contour, poly, i;
+  var candidates = [], len = contours.length, contour, poly, i;
 
   this.polys = [];
   
-  for (i = 0; i !== contours.length; ++ i){
+  for (i = 0; i < len; ++ i){
     contour = contours[i];
 
     if (contour.length >= minSize){
@@ -42,7 +69,7 @@ AR.Detector.prototype.findCandidates = function(contours, minSize, epsilon, minL
 
       this.polys.push(poly);
 
-      if ( (poly.length === 4) && ( CV.isContourConvex(poly) ) ){
+      if ( (4 === poly.length) && ( CV.isContourConvex(poly) ) ){
 
         if ( CV.minEdgeLength(poly) >= minLength){
           candidates.push(poly);
@@ -55,9 +82,9 @@ AR.Detector.prototype.findCandidates = function(contours, minSize, epsilon, minL
 };
 
 AR.Detector.prototype.clockwiseCorners = function(candidates){
-  var dx1, dx2, dy1, dy2, swap, i;
+  var len = candidates.length, dx1, dx2, dy1, dy2, swap, i;
 
-  for (i = 0; i !== candidates.length; ++ i){
+  for (i = 0; i < len; ++ i){
     dx1 = candidates[i][1].x - candidates[i][0].x;
     dy1 = candidates[i][1].y - candidates[i][0].y;
     dx2 = candidates[i][2].x - candidates[i][0].x;
@@ -74,14 +101,14 @@ AR.Detector.prototype.clockwiseCorners = function(candidates){
 };
 
 AR.Detector.prototype.notTooNear = function(candidates, minDist){
-  var notTooNear = [], dist, dx, dy, i, j, k;
+  var notTooNear = [], len = candidates.length, dist, dx, dy, i, j, k;
 
-  for (i = 0; i !== candidates.length; ++ i){
+  for (i = 0; i < len; ++ i){
   
-    for (j = i + 1; j !== candidates.length; ++ j){
+    for (j = i + 1; j < len; ++ j){
       dist = 0;
       
-      for (k = 0; k !== 4; ++ k){
+      for (k = 0; k < 4; ++ k){
         dx = candidates[i][k].x - candidates[j][k].x;
         dy = candidates[i][k].y - candidates[j][k].y;
       
@@ -99,7 +126,7 @@ AR.Detector.prototype.notTooNear = function(candidates, minDist){
     }
   }
 
-  for (i = 0; i !== candidates.length; ++ i){
+  for (i = 0; i < len; ++ i){
     if ( !candidates[i].tooNear ){
       notTooNear.push( candidates[i] );
     }
@@ -109,9 +136,9 @@ AR.Detector.prototype.notTooNear = function(candidates, minDist){
 };
 
 AR.Detector.prototype.findMarkers = function(imageSrc, candidates, warpSize){
-  var markers = [], candidate, marker, i;
+  var markers = [], len = candidates.length, candidate, marker, i;
 
-  for (i = 0; i !== candidates.length; ++ i){
+  for (i = 0; i < len; ++ i){
     candidate = candidates[i];
 
     CV.warp(imageSrc, this.homography, candidate, warpSize);
@@ -131,24 +158,23 @@ AR.Detector.prototype.getMarker = function(imageSrc, candidate){
   var width = (imageSrc.width / 7) >>> 0,
       minZero = (width * width) >> 1,
       bits = [], rotations = [], distances = [],
-      square, inc, i, j;
+      square, pair, inc, i, j;
 
-  for (i = 0; i !== 7; ++ i){
-    inc = (i === 0 || i === 6)? 1: 6;
+  for (i = 0; i < 7; ++ i){
+    inc = (0 === i || 6 === i)? 1: 6;
     
     for (j = 0; j < 7; j += inc){
       square = {x: j * width, y: i * width, width: width, height: width};
-      
       if ( CV.countNonZero(imageSrc, square) > minZero){
         return null;
       }
     }
   }
 
-  for (i = 0; i !== 5; ++ i){
+  for (i = 0; i < 5; ++ i){
     bits[i] = [];
 
-    for (j = 0; j !== 5; ++ j){
+    for (j = 0; j < 5; ++ j){
       square = {x: (j + 1) * width, y: (i + 1) * width, width: width, height: width};
       
       bits[i][j] = CV.countNonZero(imageSrc, square) > minZero? 1: 0;
@@ -160,7 +186,7 @@ AR.Detector.prototype.getMarker = function(imageSrc, candidate){
   
   pair = {first: distances[0], second: 0};
   
-  for (i = 1; i !== 4; ++ i){
+  for (i = 1; i < 4; ++ i){
     rotations[i] = this.rotate( rotations[i - 1] );
     distances[i] = this.hammingDistance( rotations[i] );
     
@@ -170,7 +196,7 @@ AR.Detector.prototype.getMarker = function(imageSrc, candidate){
     }
   }
 
-  if (pair.first !== 0){
+  if (0 !== pair.first){
     return null;
   }
 
@@ -183,13 +209,13 @@ AR.Detector.prototype.hammingDistance = function(bits){
   var ids = [ [1,0,0,0,0], [1,0,1,1,1], [0,1,0,0,1], [0,1,1,1,0] ],
       dist = 0, sum, minSum, i, j, k;
 
-  for (i = 0; i !== 5; ++ i){
+  for (i = 0; i < 5; ++ i){
     minSum = Infinity;
     
-    for (j = 0; j !== 4; ++ j){
+    for (j = 0; j < 4; ++ j){
       sum = 0;
 
-      for (k = 0; k !== 5; ++ k){
+      for (k = 0; k < 5; ++ k){
           sum += bits[i][k] === ids[j][k]? 0: 1;
       }
 
@@ -207,7 +233,7 @@ AR.Detector.prototype.hammingDistance = function(bits){
 AR.Detector.prototype.mat2id = function(bits){
   var id = 0, i;
   
-  for (i = 0; i !== 5; ++ i){
+  for (i = 0; i < 5; ++ i){
     id <<= 1;
     id |= bits[i][1];
     id <<= 1;
@@ -218,11 +244,11 @@ AR.Detector.prototype.mat2id = function(bits){
 };
 
 AR.Detector.prototype.rotate = function(src){
-  var dst = [], i, j;
+  var dst = [], len = src.length, i, j;
   
-  for (i = 0; i !== src.length; ++ i){
+  for (i = 0; i < len; ++ i){
     dst[i] = [];
-    for (j = 0; j !== src[i].length; ++ j){
+    for (j = 0; j < src[i].length; ++ j){
       dst[i][j] = src[src[i].length - j - 1][i];
     }
   }
@@ -231,10 +257,10 @@ AR.Detector.prototype.rotate = function(src){
 };
 
 AR.Detector.prototype.rotate2 = function(src, rotation){
-  var dst = [], i;
+  var dst = [], len = src.length, i;
   
-  for (i = 0; i !== src.length; ++ i){
-    dst[i] = src[ (rotation + i) % src.length ];
+  for (i = 0; i < len; ++ i){
+    dst[i] = src[ (rotation + i) % len ];
   }
 
   return dst;
